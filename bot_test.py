1f67bca22dbeb9b393400f817358e0b2948ddb61
Lukasz Hoszowski <hoszak@gmail.com>
2022-02-15 19:34:14 +0100

    ajax

---
+++ bot_test.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome('./drivers/chromedriver')
+driver.get("http://www.esky.pl")
+
+'div[class$="summary-buttons"] button[size="large"]:last-child'
+
+
+# assert "Python" in driver.title
+# elem = driver.find_element_by_name("q")
+# elem.clear()
+# elem.send_keys("pycon")
+# elem.send_keys(Keys.RETURN)
+# assert "No results found." not in driver.page_source
+# driver.close()
